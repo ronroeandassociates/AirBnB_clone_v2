a0c38fa2ffe410f04c0cc103d1178a2d6db1be78
Ronroeandassociates <3321@holbertonschool.com>
2021-11-24 14:15:09 -0600

    did it fix it

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@ USE `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost';
 
 SET
-    PASSWORD FOR 'hbnb_dev' @'localhost' = 'hbnb_dev_pwd';
+    PASSWORD FOR hbnb_dev @'localhost' = 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
