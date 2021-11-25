24985aa5f9327b67703976d06b28362e916e8fe5
Ronroeandassociates <3321@holbertonschool.com>
2021-11-24 19:21:36 -0600

    trying to fix it

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,13 +6,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
 
-GRANT
-SELECT
-<<<<<<< HEAD
-    ON performance_schema.* TO 'hbnb_dev' @'localhost';
-=======
-    ON `performance_schema`.* TO 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 FLUSH PRIVILEGES;
 
->>>>>>> b71dd744ff42ffe423c792e6f25a82c8153a4f78
