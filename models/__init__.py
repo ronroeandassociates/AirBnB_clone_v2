6a053e5034200c5fb651f3e94b2956bd4dd6d5df
RockoGilbert <evolve2122@gmail.com>
2021-11-24 18:17:53 -0600

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates storage"""
 from os import getenv
 
 from .amenity import Amenity
