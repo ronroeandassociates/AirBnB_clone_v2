28cdd3a05154e0b725a5c53db3ec9e2edce2f91c
Ronroeandassociates <3321@holbertonschool.com>
2021-12-14 11:59:59 -0600

    lets see this work

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+
 from models import storage
 from models.state import State
+from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -20,6 +22,19 @@ def states_list():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_list():
+    """ displays a HTML page with a list of cities by states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+    return render_template('8-cities_by_states.html',
+                           states=st_ct,
+                           h_1="States")
 
 
 if __name__ == "__main__":
