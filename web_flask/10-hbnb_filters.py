979fde3282b3a56e1d2d0ee21d81654c015c2428
Ronroeandassociates <3321@holbertonschool.com>
2021-12-17 09:02:13 -0600

    fixed 10

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+'''Flask app for hbnb filters'''
+
+
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
-from os import environ
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+app.url_map.strict_slashes = False
+
+
+@app.route('/hbnb_filters')
+def hbnbFilters():
+    return render_template('10-hbnb_filters.html',
+                           states=storage.all(State),
+                           amenities=storage.all(Amenity))
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def teardown(context):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filter():
-    """ HBNB filters """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    return render_template('10-hbnb_filters.html',
-                           states=st_ct,
-                           amenities=amenities)
-
-
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
