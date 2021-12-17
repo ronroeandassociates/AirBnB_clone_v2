0acc7bab8803a243933fd7d187304366524118c5
Ronroeandassociates <3321@holbertonschool.com>
2021-12-17 09:09:50 -0600

    oh well

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
 """FLASK"""
-from flask import Flask
-from flask.templating import render_template
+if __name__ == '__main__':
+    from flask import Flask
+    from flask import render_template
 
-app = Flask(__name__)
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
 
+    @app.route('/')
+    def hello_HBNB():
+        """[This stes the route for hello_hbnb]
+        """
+        return "Hello HBNB!"
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """hello HBNB"""
-    return 'Hello HBNB!'
+    @app.route('/hbnb')
+    def HBNB():
+        """[This route displays HBHB]
+        """
+        return "HBNB"
 
+    @app.route('/c/<text>')
+    def print_C(text):
+        """[/c/<text> Displays C followed by the value of the text]
+        """
+        new_str = text.replace('_', ' ')
+        return "C {}".format(new_str)
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb_only():
-    """HBNB"""
-    return 'HBNB'
+    @app.route('/python/', defaults={'text': 'is cool'})
+    @app.route('/python/<text>')
+    def print_python(text="is cool"):
+        """[Prints python and gets rid of '_'
+            if there are any]
+        Args:
+            text (str):Defaults to "is cool".
+        """
+        new_str = text.replace('_', ' ')
+        return "Python {}".format(new_str)
 
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """c_route"""
-    return 'C {}' .format(text.replace("_", " "))
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """python_route"""
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    if isinstance(n, int):
+    @app.route('/number/<int:n>')
+    def display_n(n):
+        """[display “n is a number” only if n is an integer]
+        Args:
+            n ([int]):
+        """
         return "{} is a number".format(n)
 
+    @app.route('/number_template/<int:n>')
+    def number_template(n):
+        """[This displays h1 tag]
+        """
+        return render_template('5-number.html', n=n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """number_template"""
-    if isinstance(n, int):
-        return render_template("5-number.html", n=n)
-
-
-if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
